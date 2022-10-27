2525fd74568c3da641d0ff883430b6e1d2086233
Julio Moraes <juliocesardemoraes2015@gmail.com>
2022-10-26 21:40:34 -0300

    adicao lombok entidade medico

--- src/main/java/com/br/juliomoraes/clinicameriti/model/Especialidade.java
+++ src/main/java/com/br/juliomoraes/clinicameriti/model/Especialidade.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "especialidade")
 public class Especialidade {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/br/juliomoraes/clinicameriti/model/Medico.java
+++ src/main/java/com/br/juliomoraes/clinicameriti/model/Medico.java
@@ -11,23 +11,29 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.br.juliomoraes.clinicameriti.dto.medico.MedicoDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "medico")
 public class Medico {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_medico")
+	@SequenceGenerator(name = "seq_medico", sequenceName = "medico_sequence")
 	private Long id;
 	private String nome;
 	private String crm;
@@ -39,69 +45,9 @@ public class Medico {
 	private Double valorConsulta;
 
 	@OneToMany(mappedBy = "medico")
-	private List<Consulta> consultas = new ArrayList<>();
+	private List<Consulta> consultas;
 	
 	private Long usuarioId;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCrm() {
-		return crm;
-	}
-
-	public void setCrm(String crm) {
-		this.crm = crm;
-	}
-
-	public LocalDate getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(LocalDate dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public Especialidade getEspecialidade() {
-		return especialidade;
-	}
-
-	public void setEspecialidade(Especialidade especialidade) {
-		this.especialidade = especialidade;
-	}
-
-	public Double getValorConsulta() {
-		return valorConsulta;
-	}
-
-	public void setValorConsulta(Double valorConsulta) {
-		this.valorConsulta = valorConsulta;
-	}
-
-	public List<Consulta> getConsultas() {
-		return consultas;
-	}
-	
-	public Long getUsuarioId() {
-		return usuarioId;
-	}
-
-	public void setUsuarioId(Long usuarioId) {
-		this.usuarioId = usuarioId;
-	}
 	
 	public void atualizaDadosMedico(MedicoDTO dto) {
 		this.nome = dto.getNome();
